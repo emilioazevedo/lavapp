f427e1ac6c880085e1eef37005ce91a94ff8fd03
emilioazevedo <emilio@gridmonitor.com>
2025-01-10 15:19:01 +0000

    Add Tailwind CSS configuration

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="p-4 bg-blue-500 text-white">
         <img src={reactLogo} className="logo react" alt="React logo" />
 
-        <h1>LAV APP</h1> 
+        <h1 className="text-2xl font-bold">LAV APP</h1> 
      <h2>BTC Buy and Sell Signals Hello from Amplify</h2>
       </header>
     </div>
